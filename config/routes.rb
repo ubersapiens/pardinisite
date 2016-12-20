bd0ebb71084e51a5ed9305dfa064ba8b30da6a03
Juan Pardini <jfpardini@gmail.com>
2016-12-20 20:35:15 +0100

    Blog and contact setup.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  resources :posts
   resources :contacts
   root 'welcome#home'
 
   get '/work' => 'welcome#work'
   get '/services' => 'welcome#services'
+  get '/blog' => 'posts#index'
+  get '/contact' => 'contacts#new'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161205105640) do
+ActiveRecord::Schema.define(version: 20161220193050) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 20161205105640) do
     t.text     "project_description"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
